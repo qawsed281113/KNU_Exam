09761e8295ecb40b0fd4d4e5515b31b98fc51f2a
qawsed281113 <65415696+qawsed281113@users.noreply.github.com>
2022-12-09 11:16:12 +0200

    Add files via upload

--- лаби/index.py
+++ лаби/index.py
@@ -1,27 +1,29 @@
 def get_file():
-        file = open("exam.txt")
+        file = open("exam.txt","r")
         text = file.read()
         _list = text.split("\n")
         true_list = []
     
         for i in _list:
             true_list.append(i)   
-        file.close()
-        
         return true_list
-
+        file.close()
 
 def XXX_AAA(num):
-     l_ist=get_file()
-     x=True
-     
-     for i in l_ist:
-            adres = i.split(" -- "[1])
-            for j in adres : 
-                if j > num :
-                    print(i)
-                   
+     l_ist= get_file()
+     for x,y in enumerate(l_ist):
+         l_ist[x] = y.split("--")  
+     for j in l_ist:
+        if int(j[1]) > num:
+           print("XXX  > then AAA in : ")
+           print(f"{j[0]}--{j[1]}")
+            
 def main():
-    num = input("Enter AAA number: ")
-    XXX_AAA(num)
-main()
+    while True:
+        num = input("Enter AAA number: ")
+        if num.isdigit():
+            XXX_AAA(int(num))
+            break
+        else:
+            print("Enter only numbers:")
+main() 
